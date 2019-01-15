81a01c718bc1bfc6eab80e19c9974ed83acc4c7a
h2oche <h2oche22@gmail.com>
2019-01-16 00:45:41 +0900

    init

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
-var http = require('http');
+// var http = require('http');
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello Travis!\n')
-}).listen(1337, '127.0.0.1');
+// http.createServer(function (req, res) {
+//   res.writeHead(200, {'Content-Type': 'text/plain'});
+//   res.end('Hello Travis!\n')
+// }).listen(1337, '127.0.0.1');
 
-console.log('Server running at http://127.0.0.1:1337/');
+// console.log('Server running at http://127.0.0.1:1337/');
+
+module.exports
+= {
+  addPositive: (a, b) => {
+    if(a <= 0 || b <= 0)
+      return -1;
+    return a+b;
+  },
+
+  add: (a,b) => {
+    return a+b;
+  }
+}
--- test/test.spec.js
+++ test/test.spec.js
@@ -1,9 +1,30 @@
 var assert = require('assert');
+var adder = require('../index');
 
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal([1,2,3].indexOf(4), -1);
+// describe('Array', function() {
+//   describe('#indexOf()', function() {
+//     it('should return -1 when the value is not present', function() {
+//       assert.equal([1,2,3].indexOf(4), -1);
+//     });
+//   });
+// });
+
+describe('adder', function() {
+  describe('#addPositive', function() {
+    it('should return -1 when a or b is not positive', function() {
+      assert.equal(adder.addPositive(0, 3), -1);
+      assert.equal(adder.addPositive(3, 0), -1);
+      assert.equal(adder.addPositive(-1, -1), -1);
+    });
+
+    it('should return a+b when a, b are positive', function() {
+      assert.equal(adder.addPositive(3, 4), 7);
+    });
+  });
+
+  describe('#add', function() {
+    it('should return a+b', function() {
+      assert.equal(adder.add(3, 4), 7);
     });
   });
 });
